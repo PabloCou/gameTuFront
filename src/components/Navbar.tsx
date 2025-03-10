2ac01553446e3d303fc8c8e68e8f7a0950c45306
PabloCou <pablocourou@gmail.com>
2025-03-10 18:20:58 +0100

    v0.8.3

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -60,7 +60,7 @@ function Navbar() {
       }
        {isAuthenticated &&
       <li>
-        <Link to="/profile" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-600 md:p-0 md:dark:hover:text-orange-400 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Profile</Link>
+        <Link to="/profile" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-600 md:p-0 md:dark:hover:text-orange-400 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Perfil</Link>
       </li>
       } 
       {isAdmin && 
